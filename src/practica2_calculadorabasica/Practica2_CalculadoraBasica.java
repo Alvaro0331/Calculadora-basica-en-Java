8fbf44ba68db690459586c7b2a76b2d6ecb9d148
Alvaro Davila Gomez <alvaritodavilag0331@gmail.com>
2025-02-07 18:24:11 -0600

    Inicio del proyecto

---
+++ src/practica2_calculadorabasica/Practica2_CalculadoraBasica.java
@@ -0,0 +1,16 @@
+package practica2_calculadorabasica;
+
+/**
+ *
+ * @author Alvaro Davila Gomez
+ */
+public class Practica2_CalculadoraBasica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
