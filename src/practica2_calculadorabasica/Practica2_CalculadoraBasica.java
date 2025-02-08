11d401c437f8320301a5fc608d663358d414eeb4
Alvaro Davila Gomez <alvaritodavilag0331@gmail.com>
2025-02-07 18:28:52 -0600

    Agregadas las librerias de trabajo

--- src/practica2_calculadorabasica/Practica2_CalculadoraBasica.java
+++ src/practica2_calculadorabasica/Practica2_CalculadoraBasica.java
@@ -1,16 +1,22 @@
 package practica2_calculadorabasica;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Alvaro Davila Gomez
  */
-public class Practica2_CalculadoraBasica {
-
-    /**
-     * @param args the command line arguments
-     */
+public class Practica2_CalculadoraBasica extends JFrame implements ActionListener{
+    
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
     }
     
 }
